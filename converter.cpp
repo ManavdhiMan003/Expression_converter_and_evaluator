d0ceda2225730046d56a94eeb41039fcc7075d4e
manavdhiman <65016580+ManavdhiMan003@users.noreply.github.com>
2020-11-17 18:00:26 +0530

    add infix to postfix function

--- converter.cpp
+++ converter.cpp
@@ -1,11 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define endl "\n"
-#define loop(i,n) for(i=0;i<n;i++)
+#define loop(i,n) for(int i=0;i<n;i++)
 #define loop1(i,k,n) for(i=k;i<n;i++)
 #define pb push_back
 typedef long long int ll;
-void 
+int order(char c){
+    if(c=='^') return 3;
+    else if(c=='*'||c=='/') return 2;
+    else if(c=='+'||c=='-') return 1;
+    return -1;
+}
+void infixtopostfix(string s){
+    stack<char> st;
+    int len=s.length();
+    string ans;
+    st.push('\n');
+    loop(i,len){
+        if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')) ans+=s[i];
+        else if(s[i]=='(') st.push('(');
+        else if(s[i]==')'){
+            while(st.top()!='\n'&&st.top()!='('){
+                char ch=st.top();
+                st.pop();
+                ans+=ch;
+            }
+            if(st.top()=='('){
+                st.pop();
+            }
+        }
+        else{
+            while(st.top()!='\n'&&order(s[i])<=order(st.top())){
+                char ch=st.top();
+                st.pop();
+                ans+=ch;
+            }
+            st.push(s[i]);
+        }
+    }
+    while(st.top()!='\n'){
+        char ch=st.top();
+        st.pop();
+        ans+=ch;
+    }
+    cout<<"Postfix Expression = "<<ans<<endl;
+}
 void intro(){
     cout<<"Welcome TO Expression Evalutaor\n\t\tBy: Manav Dhiman\n\n";
 }
@@ -21,14 +60,17 @@ int main(){
     cout<<"Enter 6 for Postfix to Prefix\n";
     cout<<"Enter 7 for Infix Evalutaion\n";
     cout<<"Enter 8 for Postfix Evalutaion\n";
-    cout<<"Enter 7 for Prefix Evalutaion\n";
+    cout<<"Enter 9 for Prefix Evalutaion\n";
+    cout<<"Enter 10 for exit\n";
     cin>>i;
     switch(i){
         case 1:
         cin>>s;
+        goto g;
         break;
         case 2:
         infixtopostfix(s);
+        goto g;
         break;
         case 3:
         infixtoprefix(s);
@@ -50,6 +92,8 @@ int main(){
         break;
         case 9:
         prefix_eval(s);
+        break;
+        case 10:
         break;
     }
     return 0;
